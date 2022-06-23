8cf52830e9e687f8f5c410728c7f9d4786331366
jmaen <jammi1104@gmail.com>
2022-06-23 23:59:53 +0200

    Comment code, add checkPoint()

--- src/main/kotlin/hittables/Cylinder.kt
+++ src/main/kotlin/hittables/Cylinder.kt
@@ -1,8 +1,10 @@
 package hittables
 
+import kotlin.math.sqrt
+
 import geometry.*
 import shading.Material
-import kotlin.math.sqrt
+import util.*
 
 class Cylinder(private val center1: Vector3, private val center2: Vector3, private val radius: Double, private val material: Material) : Hittable {
     private val disk1 = Disk(center1, center1 - center2, radius, material)
@@ -13,12 +15,31 @@ class Cylinder(private val center1: Vector3, private val center2: Vector3, priva
         val n = (center2 - center1).normalized()
         val d = ray.direction
 
-        val a = (d - (d dot n)*n) dot (d - (d dot n)*n)
-        val b = 2*((d - (d dot n)*n) dot (ao - (ao dot n)*n))
-        val c = ((ao - (ao dot n)*n) dot (ao - (ao dot n)*n)) - radius*radius
+        /*
+        axis equation:
+        c1 + u*(c2 - c1)
+
+        distance point - line:
+        line: x = a + u*b
+        (p - a) - ((p - a) ⋅ b)*b
+
+        cylinder equation: (vec^2 := vec ⋅ vec)
+        |(p - a) - ((p - a) ⋅ b)*b| = r
+        <=> (p - c1 - ((c2 - c1) ⋅ (p - c1))*(c2 - c1))^2 - r^2 = 0
+        <=> ((o + t*d) - c1 - ((c2 - c1) ⋅ ((o + t*d) - c1))*(c2 - c1))^2 - r^2 = 0
+        <=> (d - (d ⋅ (c2 - c1))*(c2 - c1)^2*t^2
+            + 2*((d - (d ⋅ (c2 - c1))*(c2 - c1)) ⋅ ((o - c1) - ((o - c1) ⋅ (c2 - c1))*(c2 - c1)))*t
+            + ((o - c1) - ((o - c1) ⋅ (c2 - c1))*(c2 - c1))^2 - r^2
+        */
+        val dn = d - (d dot n)*n
+        val aon = ao - (ao dot n)*n
+        val a = dn dot dn
+        val b = 2*(dn dot aon)
+        val c = (aon dot aon) - radius*radius
 
         var smallestT: Double? = null
         var normal: Vector3? = null
+
         val discriminant = b*b - 4*a*c
         if(discriminant >= 0) {  // there is at least 1 solution
             var t = (-b - sqrt(discriminant)) / (2*a)  // only the smaller t is interesting
@@ -29,28 +50,29 @@ class Cylinder(private val center1: Vector3, private val center2: Vector3, priva
                 val intersection = ray.pointAt(t)
                 val closestPoint = getClosestPointOnAxis(intersection)
 
-                // check if closest point is inside the cylinder
-                val d = (center2 - center1).length()
+                // check if the closest point on the axis is inside the cylinder (between disk centers)
+                val height = (center2 - center1).length()
                 val d1 = (closestPoint - center1).length()
                 val d2 = (closestPoint - center2).length()
-                if(d1 <= d && d2 <= d) {
+                if(d1 <= height && d2 <= height) {
                     smallestT = t
                     normal = intersection - closestPoint
                 }
             }
         }
 
-        val h1 = disk1.hit(ray, tMin, tMax)
-        val t1 = h1?.t
+        // check top and bottom disk
+        val hit1 = disk1.hit(ray, tMin, tMax)
+        val t1 = hit1?.t
         if(t1 != null && t1 in tMin..tMax && (smallestT == null || t1 < smallestT)) {
             smallestT = t1
-            normal = h1.normal
+            normal = hit1.normal
         }
-        val h2 = disk2.hit(ray, tMin, tMax)
-        val t2 = h2?.t
+        val hit2 = disk2.hit(ray, tMin, tMax)
+        val t2 = hit2?.t
         if(t2 != null && t2 in tMin..tMax && (smallestT == null || t2 < smallestT)) {
             smallestT = t2
-            normal = h2.normal
+            normal = hit2.normal
         }
 
         if(smallestT != null && normal != null) {
@@ -62,7 +84,18 @@ class Cylinder(private val center1: Vector3, private val center2: Vector3, priva
     }
 
     override fun checkPoint(point: Vector3): Boolean {
-        TODO()
+        // check top and bottom disk
+        if(disk1.checkPoint(point)) {
+            return true
+        }
+        if(disk2.checkPoint(point)) {
+            return true
+        }
+
+        // check cylinder
+        val closestPoint = getClosestPointOnAxis(point)
+        val distance = (closestPoint - point).length()
+        return distance == radius
     }
 
     private fun getClosestPointOnAxis(point: Vector3): Vector3 {
